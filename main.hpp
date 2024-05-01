6a302e20fbcf4531d050f2b19ba425ced43544e6
joselinnnnnn <157412207+joselinnnnnn@users.noreply.github.com>
2024-05-01 01:11:27 +0000

    idk whats up with those numbers

--- main.hpp
+++ main.hpp
@@ -1,8 +1,75 @@
-
+#include <iostream>
+#include <cmath>
+using namespace std;
 //********************
 // Declare Function Prototypes
 //********************
 
+void getTwoValues(int, int);
+int getNextPrime(int);
+int getPrevPrime(int);
+
+
+
 // ******************************
 // Implement all your functions here
 // ******************************
+
+void getTwoValues(int begin, int end){
+    cout << "Please enter two values" << endl;
+   cin >> begin >> end;
+    while(begin > end){
+        cout << "the first must be smaller than the second, try again";
+        cin >> begin >> end;
+    }
+  
+}
+
+int getNextPrime(int begin){
+    int maybePrime = begin + 1;
+  bool prime = false;
+  while (prime == false){
+  for (int i = 2; i <= sqrt(maybePrime); i++) {
+    if (maybePrime % i == 0) {
+      prime = false;
+      break;
+    }
+    else {
+      prime = true;
+    }
+  //  maybePrime++;
+  }
+    if(prime){
+      cout << maybePrime << endl;
+      return maybePrime;
+      break;
+    }
+     maybePrime++;
+  }
+  
+}
+
+int getPrevPrime(int end){
+       int maybePrime = end - 1;
+  bool prime = false;
+  while (prime == false){
+  for (int i = 2; i <= sqrt(maybePrime); i++) {
+    if (maybePrime % i == 0) {
+      prime = false;
+      break;
+    }
+    else {
+      prime = true;
+    }
+  //  maybePrime++;
+  }
+    if(prime){
+      cout << maybePrime << endl;
+      return maybePrime;
+      break;
+    }
+     maybePrime = maybePrime-1;
+  }
+  
+}
+
